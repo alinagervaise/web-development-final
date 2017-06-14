b42f280bcd041231d8b763798915c184bddd6588
Gervaise Alina <gervaisita@gmail.com>
2017-06-14 15:55:04 +0200

    just added 5 users

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -4,6 +4,7 @@
 -- Class table needs at least  2 values
 -- Class_member will link the user with only one class value
 -- create an evaluation for both classes
+-- e class, 2 trainers, at least 3 students, 6 row for class_member, 2 evaluation for 1 trainer in a , 2 quiz, 4 or 5 questions by quiz, 1 evaluation for 1st trainer in the 2nd clas  for 2 quiz, 1 evaluation for 1st trainer in the 1nd clas  for 1, 2 quiz; 3 tests for evaluation 1, 3 test for evluation 1 ; 1 with all answers; 1 with 3answers, 1 wtih 2 answers
 /* Reset procedure declaration and call, which populate the table */
 
 DELIMITED $
@@ -14,6 +15,9 @@ BEGIN
    INSERT INTO user(user_id,email, pwd, name, first_name)VALUES
    (1, "trainer1@gmail.com", "trainer1", "TRAINER1", "Jean"),
    (2, "trainer2@gmail.com", "trainer2", "TRAINER2", "Marie");
+   (3, "student1@gmail.com", "student1", "STUDENT1", "Paul"),
+   (4, "student2@gmail.com", "student2", "STUDENT2", "Laura");
+   (5, "student3@gmail.com", "student3", "STUDENT3", "Andrew");
   -- Reset table class
   TRUNCATE class;
   -- Create at least 2 classes
