ca612d3fedcd6089b5b6e43f3dd5cbe177119eac
Gervaise Alina <gervaisita@gmail.com>
2017-06-14 16:12:56 +0200

    just added  6 students

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -11,29 +11,51 @@ DELIMITED $
 DROP PROCEDURE IF EXISTS init_db $$
 CREATE PROCEDURE init_db()
 BEGIN
-   -- create an user
-   INSERT INTO user(user_id,email, pwd, name, first_name)VALUES
+   -- create at least 5 users
+   TRUNCATE user;
+   INSERT INTO user(user_id,email, pwd, name, first_name)
+   VALUES
    (1, "trainer1@gmail.com", "trainer1", "TRAINER1", "Jean"),
    (2, "trainer2@gmail.com", "trainer2", "TRAINER2", "Marie");
    (3, "student1@gmail.com", "student1", "STUDENT1", "Paul"),
    (4, "student2@gmail.com", "student2", "STUDENT2", "Laura");
    (5, "student3@gmail.com", "student3", "STUDENT3", "Andrew");
-  -- Reset table class
-  TRUNCATE class;
+   (6, "student4@gmail.com", "student4", "STUDENT4", "Amanda"),
+   (7, "student5@gmail.com", "student5", "STUDENT5", "Rihanna");
+   (8, "student6@gmail.com", "student6", "STUDENT6", "Louis");
+  
   -- Create at least 2 classes
-  insert into class (class_id, name) values 
+  TRUNCATE class;
+  INSERT INTO class (class_id, name)
+  VALUES  
    (1, "SQL class for French Student"),
    (2, "SQL class for International Student");
+   
+   -- Create at least 2 trainers
+  TRUNCATE trainer;
+  INSERT INTO trainer (user_id) 
+  VALUES
+   (1),
+   (2);
+  
+    -- register at least 3 students into in the class
+   TRUNCATE class_member;
+   INSERT INTO class_member(user_id,class_id)
+   VALUES
+   (3, 1),
+   (4, 1),
+   (5, 1);
+   (6, 2),
+   (7, 2),
+   (8, 2);
+   
    --- create 1 evaluation per class
    TRUNCATE evaluation
-   insert into evaluation (evaluation_id,scheduled_at,ending_at,nb_minutes,class_id);--,trainer_id, quiz_id,completed_at)
+   INSERT INTO evaluation (evaluation_id,scheduled_at,ending_at,nb_minutes,class_id,trainer_id, quiz_id,completed_at)
    values 
-   (1, "14-06-2017", "14-06-2017", 30, 1),
+   (1, "14-06-2017", "14-06-2017", 30, 1, ),
    (1, "14-06-2017", "14-06-2017", 30, 2);
   
-    -- register to a class
-   INSERT INTO class_member(user_id,class_id)VALUES
-   (1, 2);
    
 >);
 END $$
